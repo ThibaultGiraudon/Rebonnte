27f637a5c6edd04588385ad9ded398ea5fbcf229
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-09-04 11:04:01 +0200

    Add Medicine model tests -> coverage 100%

--- MediStock/Models/Medicine.swift
+++ MediStock/Models/Medicine.swift
@@ -13,13 +13,6 @@ struct Medicine: Identifiable, Codable, Equatable, Hashable {
         self.stock = stock
         self.aisle = aisle
     }
-
-    static func == (lhs: Medicine, rhs: Medicine) -> Bool {
-        return lhs.id == rhs.id &&
-               lhs.name == rhs.name &&
-               lhs.stock == rhs.stock &&
-               lhs.aisle == rhs.aisle
-    }
     
     func data() -> [String: Any] {
         let data: [String: Any] = [
